ce0cbc46ef1dd403bdc7a24db6af1f10c7e84507
Enfield-Li <1076462153@qq.com>
2022-05-02 11:58:36 +0800

    .

--- app/src/main/java/com/pure_spring/app/Student.java
+++ app/src/main/java/com/pure_spring/app/Student.java
@@ -4,6 +4,10 @@ public class Student implements Person {
 
   private String name;
 
+  public Student(String name) {
+    this.name = name;
+  }
+
   public void setName(String name) {
     this.name = name;
   }
