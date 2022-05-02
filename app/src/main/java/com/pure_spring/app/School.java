f493048f782d8e7a0ab73e3a9ce524700640b4f1
Enfield-Li <1076462153@qq.com>
2022-05-02 19:31:17 +0800

    First way

---
+++ app/src/main/java/com/pure_spring/app/School.java
@@ -0,0 +1,11 @@
+package com.pure_spring.app;
+
+import org.springframework.stereotype.Component;
+
+@Component("schoolBean")
+public class School {
+
+  public void test() {
+    System.out.println("test here...");
+  }
+}
---
+++ app/src/main/java/com/pure_spring/app/SchoolConfig.java
@@ -0,0 +1,14 @@
+package com.pure_spring.app;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+// @Configuration
+public class SchoolConfig {
+  //   @Bean
+  //   public School schoolBean() { // bean id
+  //     School school = new School();
+  //     return school;
+  //   }
+}
